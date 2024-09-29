643d8b9c8e240a8086277207ca92c54ac3830974
navneetshukl <navneetshukla824@gmail.com>
2024-09-29 18:37:44 +0530

    wip:websocket working

--- internals/interface/handler/handler.go
+++ internals/interface/handler/handler.go
@@ -1,97 +1,8 @@
-// package handler
-
-// import (
-// 	"encoding/json"
-// 	"fmt"
-// 	"log"
-// 	"net/http"
-// 	"news-api/internals/core/news"
-// 	"sync"
-// 	"time"
-
-// 	"github.com/gorilla/websocket"
-// )
-
-// type WebSocket struct {
-// 	connections []*websocket.Conn
-// 	mu          sync.Mutex
-// 	NewsUseCase news.NewsUseCase
-// }
-
-// func NewWebsocket(news news.NewsUseCase) *WebSocket {
-// 	return &WebSocket{
-// 		NewsUseCase: news,
-// 	}
-// }
-
-// func (ws *WebSocket) Add(conn *websocket.Conn) {
-// 	ws.mu.Lock()
-// 	defer ws.mu.Unlock()
-// 	ws.connections = append(ws.connections, conn)
-// }
-
-// var upgrader = websocket.Upgrader{
-// 	CheckOrigin: func(r *http.Request) bool {
-// 		return true // Allow connections from any origin
-// 	},
-// }
-
-// func (ws *WebSocket) HandleConnection(w http.ResponseWriter, r *http.Request) {
-// 	// Upgrade the HTTP connection to a WebSocket connection
-// 	conn, err := upgrader.Upgrade(w, r, nil)
-// 	if err != nil {
-// 		fmt.Println("Error during connection upgrade:", err)
-// 		return
-// 	}
-// 	ws.Add(conn)
-// 	defer conn.Close()
-// 	fmt.Println("Client connected")
-
-// 	log.Println("Total number of connections is", len(ws.connections))
-
-// 	for {
-// 		go ws.handleMessages()
-// 		time.Sleep(1*time.Minute)
-
-// 	}
-// }
-
-// func (ws *WebSocket) handleMessages() {
-// 	for {
-
-// 		n1, _ := ws.NewsUseCase.GetFirstNews()
-// 		bytes, err := json.Marshal(n1)
-// 		if err != nil {
-// 			log.Println("error in converting to byte ", err)
-// 			continue
-// 		}
-
-// 		msg := bytes
-// 		ws.broadcast(1, msg)
-// 		time.Sleep(1 * time.Minute) // Add sleep to avoid flooding
-// 	}
-// }
-
-// func (ws *WebSocket) broadcast(messageType int, msg []byte) {
-// 	ws.mu.Lock()
-// 	defer ws.mu.Unlock()
-
-// 	for _, conn := range ws.connections {
-// 		// Write to each connection one at a time
-// 		err := conn.WriteMessage(messageType, msg)
-// 		if err != nil {
-// 			log.Println("Error writing message:", err)
-// 			return
-// 		}
-// 	}
-// }
-
 package handler
 
 import (
 	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
 	"news-api/internals/core/news"
 	"sync"
@@ -99,6 +10,12 @@ import (
 
 	"github.com/gorilla/websocket"
 )
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true // Allow connections from any origin (not recommended for production)
+	},
+}
 
 type WebSocket struct {
 	connections []*websocket.Conn
@@ -113,86 +30,74 @@ func NewWebsocket(news news.NewsUseCase) *WebSocket {
 }
 
 func (ws *WebSocket) Add(conn *websocket.Conn) {
-	ws.mu.Lock()
-	defer ws.mu.Unlock()
 	ws.connections = append(ws.connections, conn)
-}
-
-var upgrader = websocket.Upgrader{
-	CheckOrigin: func(r *http.Request) bool {
-		return true // Allow connections from any origin
-	},
 }
 
 func (ws *WebSocket) HandleConnection(w http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		fmt.Println("Error during connection upgrade:", err)
+		http.Error(w, "Could not upgrade connection", http.StatusBadRequest)
 		return
 	}
+	defer conn.Close()
 	ws.Add(conn)
-	defer func() {
-		log.Println("Client disconnected")
-		ws.Remove(conn) // Ensure to remove the connection on disconnect
-		conn.Close()
-	}()
-	fmt.Println("Client connected")
 
-	log.Println("Total number of connections is", len(ws.connections))
+	ticker := time.NewTicker(2 * time.Second)
+	defer ticker.Stop()
+	//	cnt := 0
 
-	// Start handling messages for this connection
-	go ws.handleMessages(conn)
+	// Send a message to the client at regular intervals
+	// go func() {
+	// 	for {
+	// 		select {
+	// 		case <-ticker.C:
+	// 			str := fmt.Sprintf("%s,%d", "Hello from the server!", cnt)
+	// 			message := map[string]string{"message": str}
+	// 			msg, _ := json.Marshal(message)
+	// 			err := conn.WriteMessage(websocket.TextMessage, msg)
+	// 			if err != nil {
+	// 				return // Stop if there is an error
+	// 			}
+	// 			cnt++
+	// 		}
+	// 	}
+	// }()
 
-	// Keep reading messages from this connection
-	
-}
+	go ws.BroadCast(conn)
 
-func (ws *WebSocket) Remove(conn *websocket.Conn) {
-	ws.mu.Lock()
-	defer ws.mu.Unlock()
-	for i, c := range ws.connections {
-		if c == conn {
-			ws.connections = append(ws.connections[:i], ws.connections[i+1:]...)
+	//Handle incoming messages from the client
+	for {
+		_, msg, err := conn.ReadMessage()
+		if err != nil {
+			break
+		}
+
+		// Echo the received message back to the client
+		err = conn.WriteMessage(websocket.TextMessage, msg)
+		if err != nil {
 			break
 		}
 	}
 }
 
-func (ws *WebSocket) handleMessages(conn *websocket.Conn) {
+func (ws *WebSocket) BroadCast(conn *websocket.Conn) {
+	ticker := time.NewTicker(2 * time.Second)
+	defer ticker.Stop()
+	cnt := 0
+
+	// Send a message to the client at regular intervals
 	for {
-		n1, err := ws.NewsUseCase.GetFirstNews()
-		if err != nil {
-			log.Println("Error getting news:", err)
-			continue
-		}
-
-		bytes, err := json.Marshal(n1)
-		if err != nil {
-			log.Println("Error converting to byte:", err)
-			continue
-		}
-
-		msg := bytes
-		ws.broadcast(websocket.TextMessage, msg)
-
-		time.Sleep(1 * time.Minute) // Add sleep to avoid flooding
-	}
-}
-
-func (ws *WebSocket) broadcast(messageType int, msg []byte) {
-	ws.mu.Lock()
-	defer ws.mu.Unlock()
-
-	for i := 0; i < len(ws.connections); i++ {
-		conn := ws.connections[i]
-
-		// Write to each connection one at a time
-		if err := conn.WriteMessage(messageType, msg); err != nil {
-			log.Println("Error writing message:", err)
-			conn.Close() // Close the connection if there's an error
-			// Remove the closed connection
-			ws.connections = append(ws.connections[:i], ws.connections[i+1:]...)
-			i-- // Decrement i to check the new connection at this index
+		select {
+		case <-ticker.C:
+			str := fmt.Sprintf("%s,%d", "Hello from the server!", cnt)
+			message := map[string]string{"message": str}
+			msg, _ := json.Marshal(message)
+			err := conn.WriteMessage(websocket.TextMessage, msg)
+			if err != nil {
+				return // Stop if there is an error
+			}
+			cnt++
 		}
 	}
+
 }
--- internals/interface/routes.go
+++ internals/interface/routes.go
@@ -5,21 +5,11 @@ import (
 	"news-api/internals/interface/handler"
 
 	"github.com/gorilla/mux"
-	"github.com/rs/cors"
 )
 
 func Routes(WsHandler *handler.WebSocket) http.Handler {
 	r := mux.NewRouter()
 	r.HandleFunc("/ws", WsHandler.HandleConnection)
 
-	// Create a CORS handler
-	corsHandler := cors.New(cors.Options{
-		AllowedOrigins:   []string{"*"}, // Update this to specific origins in production
-		AllowedMethods:   []string{"GET", "POST", "OPTIONS"},
-		AllowedHeaders:   []string{"Content-Type"},
-		AllowCredentials: true,
-	})
-
-	// Wrap the router with CORS middleware
-	return corsHandler.Handler(r) // No need for type assertion
+	return r
 }
